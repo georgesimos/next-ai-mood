e3691c86996b9a1f3376933ca479409eaa9ecf49
georgesimos <admin@georgesimos.com>
2024-04-01 10:47:19 +0300

    clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next"
+import { ClerkProvider } from "@clerk/nextjs"
 import { Inter } from "next/font/google"
 import "./globals.css"
 
@@ -15,8 +16,26 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider
+      appearance={{
+        variables: { colorPrimary: "#000000" },
+        elements: {
+          formButtonPrimary:
+            "bg-black border border-black border-solid hover:bg-white hover:text-black",
+          socialButtonsBlockButton:
+            "bg-white border-gray-200 hover:bg-transparent hover:border-black text-gray-600 hover:text-black",
+          socialButtonsBlockButtonText: "font-semibold",
+          formButtonReset:
+            "bg-white border border-solid border-gray-200 hover:bg-transparent hover:border-black text-gray-500 hover:text-black",
+          membersPageInviteButton:
+            "bg-black border border-black border-solid hover:bg-white hover:text-black",
+          card: "bg-[#fafafa]",
+        },
+      }}
+    >
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </ClerkProvider>
   )
 }
